813b214af40eceadf79daa373226fe6ec9ed79b5
alvaro <alvaroarpal@gmail.com>
2021-04-06 20:13:33 +0200

    Added TODO

--- src/components/Landing/Nav/Nav.js
+++ src/components/Landing/Nav/Nav.js
@@ -2,7 +2,7 @@ import { Wrapper, ButtonDefault, Logo, Language } from './Nav.styled';
 
 const Nav = () => {
     return (
-
+        //TODO: Mejorar animacion botones, cursor pointers, etc
         <Wrapper>
             <Logo src="../../../../public/assets/images/logos/logo-provisional.png" alt=""/>
             <div>
