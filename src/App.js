2ac974ddd4d0954a50bb7b455b551e078f0effb9
miquel@gmail.com <miquel@gmail.com>
2021-04-12 21:20:17 +0200

    nav form finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from 'react'
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import "./components/FontAwesomeIcons";
 import "./components/Styles/Typography";
 import Landing from './containers/Landing/Landing';
+import Header from './components/Header-App/Nav';
 import './App.css';
-
+import { NavLink } from "react-router-dom";
 const App = () => {
+
     return (
         <BrowserRouter>
             <Switch>
-                <Route path='/' component={Landing} />
+                <Route path='/' exact component={Landing} />
+                <Route path='/Header' component={Header} />
                 {/*
                 <Route path='/dashboard' exact component={} />
            
@@ -16,8 +20,8 @@ const App = () => {
                 }
                 <Route render={() => <h1>Not found</h1>} />
             </Switch>
+            <NavLink to='/Header'>Header</NavLink>
         </BrowserRouter>
-
     );
 }
 /* si lees esto me debes un piti*/
