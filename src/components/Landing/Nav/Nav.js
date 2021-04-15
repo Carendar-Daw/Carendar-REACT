7f241fd5f083e6f19c19b11a4f4a630a9c472556
miquel@gmail.com <miquel@gmail.com>
2021-04-15 22:48:05 +0200

    little changes

--- src/components/Landing/Nav/Nav.js
+++ src/components/Landing/Nav/Nav.js
@@ -2,7 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Wrapper, Button, Logo, Language } from './Nav.styled';
 
 const Nav = () => {
-    const { loginWithRedirect, logout } = useAuth0();
+    const { loginWithRedirect } = useAuth0();
 
     return (
         <Wrapper>
