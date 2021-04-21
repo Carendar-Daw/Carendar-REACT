86cb86c66a804b44a07537a1626a816e3651e1a5
alvaro <alvaroarpal@gmail.com>
2021-04-21 17:19:14 +0200

    Added calendar and drawer

---
+++ src/components/Calendar/Calendar/Calendarapp.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { device } from '@Components/Styles/Devices';
+
+export const Container = styled.div`
+width: 100%;
+`;
--- src/components/Calendar/Cosa/Cosa.js
+++ src/components/Calendar/Cosa/Cosa.js
@@ -4,7 +4,6 @@ import React from 'react';
 const Cosa = () => {
     return (
         <>
-        <h1>Viva españa</h1>
         </>
     )
 
