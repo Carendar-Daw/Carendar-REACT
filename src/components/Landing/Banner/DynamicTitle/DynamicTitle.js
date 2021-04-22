295b2f56ab322472b3dfb17891d2fb2bcafc1a63
marc villar colomer <marc_villarcolomer@iescarlesvallbona.cat>
2021-04-22 19:06:34 +0200

    titulo fixed me cago en dios

--- src/components/Landing/Banner/DynamicTitle/DynamicTitle.js
+++ src/components/Landing/Banner/DynamicTitle/DynamicTitle.js
@@ -5,30 +5,28 @@ const DynamicTitle = () => {
   const { messages, language } = useContext(I18nContext);
   const title = [`${messages[language].Welcome.WelcomeBanner}`];
   const subtitle = [`${messages[language].Welcome.WelcomeSubTitle}`];
-  const [index, setIndex] = useState(0);
+  const [index] = useState(0);
   const [subIndex, setSubIndex] = useState(0);
+  const [subIndexSub, setSubIndexSub] = useState(0);
 
   // Title
   useEffect(() => {
-    if (index === title.length) return;
-
-    if (subIndex === title[index].length + 1
-      && index !== title.length - 1) {
-      return;
-    }
-
-    if (subIndex === 0 && subIndex >= title.length) {
-      setIndex((prev) => prev + 1);
-      return;
-    }
-
     const timeout = setTimeout(() => {
       setSubIndex((prev) => prev + 1);
-    }, (150));
+    }, (158));
 
     // eslint-disable-next-line consistent-return
     return () => clearTimeout(timeout);
   }, [subIndex, index]);
+
+  useEffect(() => {
+    const timeoutSub = setTimeout(() => {
+      setSubIndexSub((prev) => prev + 1);
+    }, (73));
+
+    // eslint-disable-next-line consistent-return
+    return () => clearTimeout(timeoutSub);
+  }, [subIndexSub, index]);
 
   return (
     <>
@@ -36,7 +34,7 @@ const DynamicTitle = () => {
         {`${title[index].substring(0, subIndex)}`}
       </h1>
       <h2 className="subtitulo">
-      {`${subtitle[index].substring(0, subIndex)}`}
+        {`${subtitle[index].substring(0, subIndexSub)}`}
       </h2>
     </>
   );
--- src/components/Landing/Section/Section.js
+++ src/components/Landing/Section/Section.js
@@ -1,33 +1,34 @@
+import React, { useContext } from 'react';
+import {
+  Container, Card, Title, SubTitle,
+} from './Section.styled';
+import { I18nContext } from '../../../config/language';
 
-import { Container, Card, Title, SubTitle, Text } from './Section.styled';
-import React, { useContext } from "react";
-import { I18nContext } from "../../../config/language";
-
-
-
-const { messages, language } = useContext(I18nContext);
-return (
+const Section = () => {
+  const { messages, language } = useContext(I18nContext);
+  return (
     <Container>
-        <Card>
-            <div>
-                <Title>{messages[language].Section.Title_Left}</Title>
-                <SubTitle>{messages[language].Section.Subtitle_Left}</SubTitle>
-            </div>
-            <div>
-                <img src="../../../../public/assets/images/logos/logo-carendar.png" alt="" />
-            </div>
-        </Card>
+      <Card>
+        <div>
+          <Title>{messages[language].Section.Title_Left}</Title>
+          <SubTitle>{messages[language].Section.Subtitle_Left}</SubTitle>
+        </div>
+        <div>
+          <img src="../../../../public/assets/images/logos/logo-carendar.png" alt="" />
+        </div>
+      </Card>
 
-        <Card reverse>
-            <div>
-                <Title>{messages[language].Section.Title_Right}</Title>
-                <SubTitle>{messages[language].Section.Subtitle_Right}</SubTitle>
-            </div>
-            <div>
-                <img src="../../../../public/assets/images/logos/logo-carendar.png" alt="" />
-            </div>
-        </Card>
+      <Card reverse>
+        <div>
+          <Title>{messages[language].Section.Title_Right}</Title>
+          <SubTitle>{messages[language].Section.Subtitle_Right}</SubTitle>
+        </div>
+        <div>
+          <img src="../../../../public/assets/images/logos/logo-carendar.png" alt="" />
+        </div>
+      </Card>
     </Container>
-);
+  );
+};
 
 export default Section;
