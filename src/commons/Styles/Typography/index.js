c3901869b1f2c467636b7712ed5aa5ccbabfd744
alvaro <alvaroarpal@gmail.com>
2021-04-23 21:22:47 +0200

    Fixed fonts

--- src/commons/Styles/Typography/index.js
+++ src/commons/Styles/Typography/index.js
@@ -1,28 +1,8 @@
-const typography = {
-  bebasNeue: '/public/assets/fonts/BebasNeue-Regular/BebasNeue-Regular.ttf',
-  montserrat: '/public/assets/fonts/Montserrat/Montserrat-Regular.ttf',
-  poppins: '/public/assets/fonts/Poppins/Poppins-Regular.ttf',
-  robotoSlab: '/public/assets/fonts/Roboto_Slab/static/RobotoSlab-Regular.ttf',
-
-};
-
 const Fonts = {
-  bebasNeue: `@font-face {
-  font-family: BebasNeue-Regular;
-  src: url(${typography.bebasNeue});
-}`,
-  montserrat: `@font-face {
-  font-family: Montserrat-Regular;
-  src: url(${typography.montserrat});
-}`,
-  poppins: `@font-face {
-  font-family: Poppins-Regular;
-  src: url(${typography.poppins});
-}`,
-  robotoSlab: `@font-face {
-  font-family: RobotoSlab-Regular;
-  src: url(${typography.robotoSlab});
-}`,
+  bebasNeue: 'font-family: BebasNeue-Regular;',
+  montserrat: 'font-family: \'Montserrat\';',
+  poppins: 'font-family: Poppins-Regular;',
+  robotoSlab: 'font-family: RobotoSlab-Regular;',
 };
 
 export default Fonts;
