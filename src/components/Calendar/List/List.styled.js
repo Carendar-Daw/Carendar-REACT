1077d918a510e0e3e9aa75d03ea666f5e010ad56
alvaro <alvaroarpal@gmail.com>
2021-04-27 21:19:11 +0200

    List component

---
+++ src/components/Calendar/List/List.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { CardApp } from '@Commons/Styles/Style/Style.styled';
+
+const Container = styled(CardApp)`
+    padding: 30px;  
+    width: 25%;
+  
+`;
+
+export default Container;
