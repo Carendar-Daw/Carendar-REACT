65020fe6217edae340c7514e88b142a4e547d36c
alvaro <alvaroarpal@gmail.com>
2021-05-15 20:48:51 +0200

    Added status filtering

--- src/pages/Cash/Filters/Filters.js
+++ src/pages/Cash/Filters/Filters.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { WrapperComponents, WrapperFilters } from '@Pages/Cash/Filters/Filters.styled';
+import Checkbox from 'antd/es/checkbox/Checkbox';
+import { Col } from 'antd';
 
-const Filter = () => {
-  const [view, setView] = useState(false);
+const Filter = ({ appointments, setFilteredAppointments }) => {
+  const filterList = (e) => ((e.length > 0) ? setFilteredAppointments(appointments.filter((app) => e.includes(app.status))) : setFilteredAppointments(appointments));
   return (
     <>
       <WrapperFilters>
+        <h1>Filtros</h1>
         <WrapperComponents>
-          <label htmlFor="" />
-          <input type="text" />
-          <label htmlFor="" />
-          <input type="text" />
-          <label htmlFor="" />
-          <input type="text" />
-          <label htmlFor="" />
-          <input type="text" />
-          <label htmlFor="" />
-          <input type="text" />
-          <label htmlFor="" />
-          <input type="text" />
+          <Checkbox.Group onChange={filterList} style={{ width: '100%' }}>
+            <Col span={8}>
+              <Checkbox value="Pendiente">Pendiente</Checkbox>
+            </Col>
+            <Col span={8}>
+              <Checkbox value="Aprobado">Aprobado</Checkbox>
+            </Col>
+            <Col span={8}>
+              <Checkbox value="Finalizado">Finalizado</Checkbox>
+            </Col>
+            <Col span={8}>
+              <Checkbox value="Facturado">Facturado</Checkbox>
+            </Col>
+            <Col span={8}>
+              <Checkbox value="Cancelado">Cancelado</Checkbox>
+            </Col>
+          </Checkbox.Group>
         </WrapperComponents>
       </WrapperFilters>
     </>
--- src/pages/Cash/Table/Table.js
+++ src/pages/Cash/Table/Table.js
@@ -1,99 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Table } from 'antd';
-import axios from '@Commons/http';
-import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Modal from 'antd/es/modal/Modal';
 
-const columns = [
-  {
-    title: 'PK',
-    dataIndex: 'key',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Status',
-    dataIndex: 'status',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Customer',
-    className: 'customer',
-    dataIndex: 'customer',
-    align: 'right',
-  },
-  {
-    title: 'Price',
-    dataIndex: 'price',
-  },
-  {
-    title: 'Date',
-    dataIndex: 'date',
-  },
-  {
-    title: 'Services',
-    dataIndex: 'services',
-  },
-  {
-    title: 'Cobrar',
-    dataIndex: 'payment',
-    align: 'center',
-    render: (appointment) => (
-      <FontAwesomeIcon onClick={() => console.log(appointment)} className="icon" icon="shopping-cart" />
-    ),
-  },
-];
+const TableCash = ({ appointments }) => {
+  const [visible, setVisible] = useState(false);
+  const columns = [
+    {
+      title: 'PK',
+      dataIndex: 'key',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Customer',
+      className: 'customer',
+      dataIndex: 'customer',
+      align: 'right',
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'date',
+    },
+    {
+      title: 'Services',
+      dataIndex: 'services',
+    },
+    {
+      title: 'Cobrar',
+      dataIndex: 'payment',
+      align: 'center',
+      render: (appointment) => (
+        <FontAwesomeIcon onClick={() => setVisible(true)} className="icon" icon="shopping-cart" />
+      ),
+    },
+  ];
 
-const TableCash = () => {
-  const [appointments, setAppointments] = useState([]);
+  const renderModal = () => {
 
-  const hasServices = (app) => {
-    const sad = app.map((appointment) => {
-      const x = app.filter((a) => a.app_id === appointment.app_id);
-      if (x.length > 1) {
-        const ayuda = { ...x[0] };
-        x.forEach((ele, index) => {
-          if (index !== 0) {
-            ayuda.ser_description = `${ayuda.ser_description}, ${ele.ser_description}`;
-            ayuda.ser_price += ele.ser_price;
-          }
-        });
-        return ayuda;
-      }
-      return appointment;
-    });
-
-    return sad.filter(
-      (v, i, a) => a.findIndex((t) => t.app_id === v.app_id) === i,
-    );
   };
 
-  useEffect(async () => {
-    const allEvents = [];
-    const response = await axios.get('/appointment/cash');
-    const allAppointments = hasServices(response.data.appointments);
-    allAppointments.forEach((app) => {
-      const event = {
-        key: app.app_id,
-        status: app.app_state,
-        customer: app.cus_name,
-        price: app.ser_price,
-        date: app.app_date,
-        services: app.ser_description,
-        payment: app,
-      };
-      allEvents.push(event);
-    });
-    setAppointments(allEvents);
-  }, []);
-
   return (
-    <Table
-      columns={columns}
-      dataSource={appointments}
-      bordered
-      title={() => 'Header'}
-      footer={() => 'Footer'}
-    />
+    <>
+      <Table
+        columns={columns}
+        bordered
+        dataSource={appointments}
+        title={() => <p>Header</p>}
+        footer={() => 'Footer'}
+      />
+      <Modal
+        title="Title"
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+      >
+        <p>{renderModal}</p>
+      </Modal>
+    </>
+
   );
 };
 
