ebeddbea245ad415166f6f1025f80c3b45c76317
alvaro <alvaroarpal@gmail.com>
2021-05-17 19:07:15 +0200

    Hotfix dependencies problem

--- src/pages/Statistics/products/ProductsStatistics.js
+++ src/pages/Statistics/products/ProductsStatistics.js
@@ -1,28 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { WrapperPie, Title, SubTitle } from './productsStadistics.styled';
+import { WrapperPie, Title, SubTitle } from './ProductsStadistics.styled';
 
 const productsStatiscs = ({ products, loadingSpinner }) => {
-    const [isData, setIsData] = useState(false);
+  const [isData, setIsData] = useState(false);
 
-    useEffect(() => {
-        if (products) setIsData(true);
-    }, [loadingSpinner]);
+  useEffect(() => {
+    if (products) setIsData(true);
+  }, [loadingSpinner]);
 
-    return (
-        <WrapperPie>
-            <Title>Products</Title>
-            {isData ?
-                (
-                    <>
-                        <SubTitle>Your products stadistics</SubTitle>
-                        <p>Has gastado {products} $ en productos.</p>
-                    </>
-                ) : (
-                    <p>Choose some date...</p>
-                )}
+  return (
+    <WrapperPie>
+      <Title>Products</Title>
+      {isData
+        ? (
+          <>
+            <SubTitle>Your products stadistics</SubTitle>
+            <p>
+              Has gastado
+              {products}
+              {' '}
+              $ en productos.
+            </p>
+          </>
+        ) : (
+          <p>Choose some date...</p>
+        )}
 
-        </WrapperPie>
-    );
+    </WrapperPie>
+  );
 };
 
 export default productsStatiscs;
