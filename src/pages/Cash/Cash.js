381831f8e5e9863c1ed8bea92fb5f3ced9efc5f9
alvaro <alvaroarpal@gmail.com>
2021-05-23 20:52:04 +0200

    Fixed merge

--- src/pages/Cash/Cash.js
+++ src/pages/Cash/Cash.js
@@ -9,38 +9,32 @@ const Cash = () => {
   const [appointments, setAppointments] = useState([]);
   const [filteredAppointments, setFilteredAppointments] = useState([]);
   const [loadingSpinner, setLoadingSpinner] = useState(false);
-<<<<<<< HEAD
-=======
-
-  useEffect(async () => {
-    await getAppointmentsCash();
-  }, []);
-
->>>>>>> dev
-  const hasServices = (app) => {
-    const sad = app.map((appointment) => {
-      const x = app.filter((a) => a.app_id === appointment.app_id);
-      if (x.length > 1) {
-        const ayuda = { ...x[0] };
-        x.forEach((ele, index) => {
-          if (index !== 0) {
-            ayuda.ser_description = `${ayuda.ser_description}, ${ele.ser_description}`;
-            ayuda.ser_price += ele.ser_price;
-          }
-        });
-        return ayuda;
-      }
-      return appointment;
-    });
-
-    return sad.filter(
-      (v, i, a) => a.findIndex((t) => t.app_id === v.app_id) === i,
-    );
-  };
 
   const getAppointmentsCash = async () => {
     const allEvents = [];
     const response = await axios.get('/appointment/cash');
+
+    const hasServices = (app) => {
+      const sad = app.map((appointment) => {
+        const x = app.filter((a) => a.app_id === appointment.app_id);
+        if (x.length > 1) {
+          const ayuda = { ...x[0] };
+          x.forEach((ele, index) => {
+            if (index !== 0) {
+              ayuda.ser_description = `${ayuda.ser_description}, ${ele.ser_description}`;
+              ayuda.ser_price += ele.ser_price;
+            }
+          });
+          return ayuda;
+        }
+        return appointment;
+      });
+
+      return sad.filter(
+        (v, i, a) => a.findIndex((t) => t.app_id === v.app_id) === i,
+      );
+    };
+
     const allAppointments = hasServices(response.data.appointments);
     allAppointments.forEach((app) => {
       const event = {
@@ -58,12 +52,16 @@ const Cash = () => {
     setFilteredAppointments(allEvents);
   };
 
+  useEffect(async () => {
+    await getAppointmentsCash();
+  }, []);
+
   return (
     <>
       {loadingSpinner && <Spinner />}
       <WrapperComponents>
         <Filters appointments={appointments} setAppointments={setAppointments} filteredAppointments={filteredAppointments} setFilteredAppointments={setFilteredAppointments} />
-        <List setLoadingSpinner={setLoadingSpinner} appointments={appointments} filteredAppointments={filteredAppointments} getAppointmentsCash={getAppointmentsCash}/>
+        <List setLoadingSpinner={setLoadingSpinner} appointments={appointments} filteredAppointments={filteredAppointments} getAppointmentsCash={getAppointmentsCash} />
       </WrapperComponents>
     </>
 
